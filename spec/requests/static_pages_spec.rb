b6cd9fa4a8fa5279c03f6406ce2e23fb2ffedd30
Sam Kreter <sakfy6@mail.missouri.edu>
2013-10-28 20:11:08 -0500

    fixed the test; add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,9 +8,14 @@ require 'spec_helper'
 			expect(page).to have_content('Sample App')
 		end
 
-		it "should have the title 'Home'" do 
+		it "should have the base title" do 
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Sample App")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should not have the custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title(' | Home')
 		end
 	end
 
@@ -35,7 +40,7 @@ require 'spec_helper'
 
   	it "should have the title 'About Us'" do 
   		visit '/static_pages/about'
-  		expect(page).to have_title('Ruby on Rails Tutorial Sample App | About us')
+  		expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
   end
 end
